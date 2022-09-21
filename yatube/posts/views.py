636ad2ebfe9dc6f6081a9420485eaa6f89662aeb
Арсен Григорян <arsen8577711@yandex.ru>
2022-09-21 20:25:29 +0300

    Исправил замечания

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,7 +1,7 @@
 from django.views.decorators.cache import cache_page
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Post, Group, User, Comment, Follow
+from .models import Post, Group, User, Follow
 from .forms import PostForm, CommentForm
 from .ulits import get_paginated_post
 
